72b9c5925d9c8e5bf14ef2ffbfdba225ee446f7b
Kelly Selden <kellyselden@gmail.com>
2019-08-18 19:18:59 +0100

    linting

---
+++ files/.eslintrc.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  overrides: [
+    {
+      files: ['vendor/**/*.js'],
+      parserOptions: {
+        ecmaVersion: 5,
+        sourceType: 'script'
+      },
+      env: {
+        amd: true
+      },
+      globals: {
+        Ember: 'readonly'
+      },
+      rules: Object.keys(Object.assign({},
+        // eslint-disable-next-line node/no-extraneous-require
+        require(path.resolve(path.dirname(require.resolve('eslint')), '../conf/eslint-recommended')).rules,
+        require('eslint-plugin-ember').configs.recommended.rules
+      )).reduce((rules, rule) => {
+        rules[rule] = 'off';
+        return rules;
+      }, {})
+    }
+  ]
+};
