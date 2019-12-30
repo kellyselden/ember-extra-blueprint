c86780d94b2be7748d1ca3100379393bb8c4b88d
Kelly Selden <kellyselden@gmail.com>
2019-12-30 13:43:03 +0000

    remove unused code

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  locals(options) {
-    let name = options.entity.name;
-
-    return {
-      name
-    };
-  }
+  name: require('./package').name
 };
